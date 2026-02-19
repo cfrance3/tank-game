2c85bf6fe0ba274fb9e9f3fc15010a2f279ba028
Caden France <160537486+cfrance3@users.noreply.github.com>
2026-02-18 19:34:17 -0600

    Added test mine scene. TODO: Implement mine collision and effects

---
+++ Scenes/mine.gd
@@ -0,0 +1,15 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	add_to_group("mine")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _explode() -> void:
+	pass
+	#TODO: implement explosion
